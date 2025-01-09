92d5f8c6a37648d79e5e2ccd595e2b121ceb2d1f
spark2 <104690611+Newsujin@users.noreply.github.com>
2025-01-09 15:27:36 +0900

    Title: 달에서 무게 구하기, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250109/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double gravitaion = 0.165;
+
+        System.out.printf("%d * %.6f = %.6f", chu, gravitaion, chu * gravitaion);
+    }
+}
